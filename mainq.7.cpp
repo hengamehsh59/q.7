c274a324bcac3df9141233e81720eb9f6f25db5a
hengamehsh59 <hengamehsh59@gmail.com>
2024-06-08 21:33:52 +0330

    Add files via upload

---
+++ mainq.7.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#define pi 3.14
+using namespace std;
+float masahat(int n)
+{
+    return pi*n*n;
+}
+float mohit(int a)
+{
+    return pi*a*2;
+}
+int main()
+{
+    int r;
+    cout<<"enter Radius";
+    cin>>r;
+    cout<<"Area of circle is:"<<masahat(r)<<endl;
+    cout<<"perimeter of circle is:"<<mohit(r);
+    return 0;
+}
